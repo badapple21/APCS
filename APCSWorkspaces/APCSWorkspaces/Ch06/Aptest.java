bf04bfed3064da22bd6cb913ad5caf9008e201f1
Your Name <mjn2024@gmail.com>
2023-10-24 15:10:24 -0400

    fixed bets fit

---
+++ APCSWorkspaces/APCSWorkspaces/Ch06/Aptest.java
@@ -0,0 +1,7 @@
+public class Aptest {
+    public static void main(String[] args) {
+        int days = 212;
+        final int DAYS_PER_WEEK = 7;
+        System.out.println(days + " days is " +   (double)days/DAYS_PER_WEEK);
+    }
+}
--- APCSWorkspaces/APCSWorkspaces/Ch06/BestFit.java
+++ APCSWorkspaces/APCSWorkspaces/Ch06/BestFit.java
@@ -5,9 +5,9 @@ public class BestFit {
         int rtn;
         if (size1 + size2 <= space) {
             rtn = 3;
-        } else if (size1 <= space && size1 > size2) {
+        } else if (size1 <= space && (size2 > space || size1 >= size2)) {
             rtn = 1;
-        } else if (size2 <= space && size2 > size1) {
+        } else if (size2 <= space && (size2 > size1 || size1 >= space)) {
             rtn = 2;
         } else {
             rtn = 0;
@@ -19,14 +19,15 @@ public class BestFit {
     public static void main(String[] args) {
         Scanner kboard = new Scanner(System.in);
         while (true) {
-            System.out.print("Please enter space: ");
-            int space = kboard.nextInt();
 
             System.out.print("1: ");
             int s1 = kboard.nextInt();
 
             System.out.print("2: ");
             int s2 = kboard.nextInt();
+
+            System.out.print("Please enter space: ");
+            int space = kboard.nextInt();
 
             System.out.println(findBestFit(s1, s2, space));
         }
